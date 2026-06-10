e8b44b8b9a214daba25a952691abb82defeb9783
loong <kimloongchen@qq.com>
2026-06-10 09:42:12 +0800

    修复 Jest 无法解析 react-native-vector-icons ES module

--- Android/app/jest.config.js
+++ Android/app/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: '@react-native/jest-preset',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native|@react-native-async-storage)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native|@react-native-async-storage|react-native-vector-icons)/)',
   ],
 };
